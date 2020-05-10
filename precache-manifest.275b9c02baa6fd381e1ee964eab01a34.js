71feeee664a7966e448580c79d990b686c8c068c
Shahmeer Shahid <shahmeer.shahid@mail.utoronto.ca>
2020-05-10 03:08:44 -0400

    Updates

---
+++ precache-manifest.275b9c02baa6fd381e1ee964eab01a34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebb0d36f61b1fc7c7fa67baa0ca9dd4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0daa82ab0385ef370ade",
+    "url": "/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "b116e381d41118026a5b",
+    "url": "/static/js/2.289eee99.chunk.js"
+  },
+  {
+    "revision": "95c85eda1a6bdffc5b337e6242a5bd6c",
+    "url": "/static/js/2.289eee99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0daa82ab0385ef370ade",
+    "url": "/static/js/main.ade4fd94.chunk.js"
+  },
+  {
+    "revision": "f505ac1f76dea84b2ab0",
+    "url": "/static/js/runtime-main.a07d0429.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
