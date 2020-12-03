993bd2cd67c0a7e8f48343f91d2100c8de020b39
llolic <lazarlolic12@gmail.com>
2020-12-03 09:35:38 -0500

    Updates

---
+++ precache-manifest.d8d1e7d70d25d15b11454a9bb1e69358.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e2d72052f6bdc06ee86de6f48f5ba9e",
+    "url": "/ResumeWords/index.html"
+  },
+  {
+    "revision": "fa192e3f9335ec649b9e",
+    "url": "/ResumeWords/static/css/main.26aedc8e.chunk.css"
+  },
+  {
+    "revision": "a3b7c2eaa24aed73079a",
+    "url": "/ResumeWords/static/js/2.05d6bcbb.chunk.js"
+  },
+  {
+    "revision": "8f5425c5c2032ef521d48b72696053de",
+    "url": "/ResumeWords/static/js/2.05d6bcbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa192e3f9335ec649b9e",
+    "url": "/ResumeWords/static/js/main.08619240.chunk.js"
+  },
+  {
+    "revision": "b6cd61dda8e21bb560ef",
+    "url": "/ResumeWords/static/js/runtime-main.f011faf6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ResumeWords/precache-manifest.1054e08ff7552352d49d4d1d5656c23f.js"
+  "/ResumeWords/precache-manifest.d8d1e7d70d25d15b11454a9bb1e69358.js"
 );
 
 self.addEventListener('message', (event) => {
